9849fa2bfd98c4c4579ea15df3e2297ac5ea49de
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-04-08 10:48:40 +0200

    increased delay

--- examples/main.py
+++ examples/main.py
@@ -1,13 +1,13 @@
 from OC10 import OC10
 from microbit import *
 
-OC10 = OC10.OC10()
+OC10 = OC10()
 
 # start OC10
 OC10.init()
 
 # sleep time
-DELAY = 500
+DELAY = 5000
 
 # infinite loop
 while True:
